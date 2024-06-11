0c8fb080f5d564b3a6944ef95ef4c4e4a5514efc
solakksk <56680735+solakksk@users.noreply.github.com>
2024-06-11 09:14:14 +0000

    init project

---
+++ stm32f103c8t6-project/build/debug/CMakeFiles/stm32f103c8t6-project.dir/Startup/startup_stm32f103c8tx.s.d
@@ -0,0 +1,2 @@
+CMakeFiles/stm32f103c8t6-project.dir/Startup/startup_stm32f103c8tx.s.obj: \
+ /workspaces/stm32f103c8t6-vscode-project/stm32f103c8t6-project/Startup/startup_stm32f103c8tx.s
---
+++ stm32f103c8t6-project/build/debug/CMakeFiles/stm32f103c8t6-project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stm32f103c8t6-project.dir/Src/main.c.obj"
+  "CMakeFiles/stm32f103c8t6-project.dir/Src/syscall.c.obj"
+  "CMakeFiles/stm32f103c8t6-project.dir/Src/sysmem.c.obj"
+  "CMakeFiles/stm32f103c8t6-project.dir/Startup/startup_stm32f103c8tx.s.obj"
+  "stm32f103c8t6-project.elf"
+  "stm32f103c8t6-project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/stm32f103c8t6-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
